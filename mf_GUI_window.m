7b8fda3a714b82a1c5a4956fff2c941d82cf5bda
Elizabeth R Louden <edewaard@nd.edu>
2018-02-18 13:15:44 -0500

    GUI Modifications
    Modified the GUI window and callbacks scripts.  Main changes reflect the new data structure organization:
    Data Structure - mf_fitter
    Main fields
    user_inputs - this is for variables input by the user in the various text boxes of the GUI.  It includes the control parameter, intensity cut-off, peak separation cut-off, fitting reference files, and smoothing preferences.
    fit_data - this will store all of the fitted parameters after each cycle of the fitting algorithm. While this will vary depending on what function the user has decided to fit, for the most common purposes it will contain the background, intensity, fwhm, and center for two or three peaks along with error from the 90% confidence interval.
    algorithm_options - again, this will vary has more algorithms are incorporated.  Currently it stores checkbox selection on which of the fitting cycles the user wants to have displayed vs running in the background.
    save_options - this stores the extension / folder string for saving files, as well as what file types (.jpg, .fig, .eps, .pdf) the user wants graphs/figures to be saved as.
    handles - this stores all of the handles pointing to the various objects such as the main GUI window and all of the text boxes for user inputs.
    numors - stores the data file numbers loaded into GRASP over which mf_fitter is to be run
    depth - stores the maximum number of files, this variable seems unnecessary and could be accomplished by replacing "mf_fitter.depth" with length(mf_fitter.numors) everywhere in a future version.
    
    Things that still need to be fixed in these files in a future version:
    - add a switch so the sector box can apply to I vs Q and I vs Xi fittings
    - and a # of peaks text box or check box next to the reference files instead of assuming it is one or two
    - there are some missing algorithm scripts in the fitting algorithm drop down (i.e. Raster Scan)
    - open the curve fit control window automatically during intialization
    - incorporate mf_fitter_plotMaker.m when that script is complete
    - remove any duplicate GRASP functions (i.e. may sector_box_callbacks2) that are unnecessary and lingering from a prior version

--- mf_GUI_window.m
+++ mf_GUI_window.m
@@ -13,15 +13,18 @@ global mf_fitter
 if ishandle(grasp_handles.window_modules.sector.window); delete(grasp_handles.window_modules.sector.window); end
 grasp_handles.window_modules.sector.inner_radius
 
+
 %% Initialize
+
 mf_GUI_callbacks('initialize');
 
+
 %% Draw Window
-% monitor:  
-%fig_position = [-1250, 580, 310, 310];
-% laptop:
+
 fig_position = [1000, 580, 350, 310];
 
+% In order to integrate nicely with the rest of GRASP
+% it is necessary to replace the sector window with MFF
 grasp_handles.window_modules.sector.window = figure(....
     'units','pixels',....
     'Position',fig_position,....
@@ -33,95 +36,85 @@ grasp_handles.window_modules.sector.window = figure(....
     'resize','off',....
     'closerequestfcn','sector_callbacks2(''close'');closereq');
 
+% Store the GUI handle in the MFF data structure
 handle = grasp_handles.window_modules.sector.window;
+mf_fitter.handles.GUI = handle;
 
+% The GUI has two main panels - the sector box definition and the smoothing parameters input
 panel = uipanel(handle, 'Title', 'Sector', 'Units', 'normalized','FontSize',10,'BackgroundColor',grasp_env.background_color,'Position',[.05 .27 .5 .7], 'ForegroundColor', [1 1 1]);
 panel2 = uipanel(handle, 'Title', 'Smoothing', 'Units', 'normalized','FontSize',10,'BackgroundColor',grasp_env.background_color,'Position',[.05 .05 .5 .2], 'ForegroundColor', [1 1 1]);
 
+
 %% Sector Box & Binning
-%Inner radius
+
+% Inner radius
 uicontrol(panel,'units','normalized','Position',[0.05,0.85,0.5,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','center','Style','text','String','Inner Radius:','BackgroundColor',grasp_env.background_color, 'ForegroundColor', [1 1 1]);
 grasp_handles.window_modules.sector.inner_radius = uicontrol(panel,'units','normalized','Position',[0.6,0.85,0.35,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.analysis_modules.sectors.inner_radius),'HorizontalAlignment','left','Visible','on','CallBack','sector_callbacks2(''inner_radius'');');
 
-%Outer Radius
+% Outer Radius
 uicontrol(panel,'units','normalized','Position',[0.05,0.73,0.51,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','center','Style','text','String','Outer Radius:','BackgroundColor',grasp_env.background_color, 'ForegroundColor', [1 1 1]);
 grasp_handles.window_modules.sector.outer_radius = uicontrol(panel,'units','normalized','Position',[0.6,0.73,0.35,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.analysis_modules.sectors.outer_radius),'HorizontalAlignment','left','Visible','on','CallBack','sector_callbacks2(''outer_radius'');');
 
-%Theta
+% Theta
 uicontrol(panel,'units','normalized','Position',[0.05,0.54,0.5,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','center','Style','text','String','Theta:','BackgroundColor',grasp_env.background_color, 'ForegroundColor', [1 1 1]);
 grasp_handles.window_modules.sector.theta = uicontrol(panel,'units','normalized','Position',[0.6,0.54,0.35,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.analysis_modules.sectors.theta),'HorizontalAlignment','left','Visible','on','CallBack','sector_callbacks2(''theta'');');
 
-%Delta Theta
+% Delta Theta
 uicontrol(panel,'units','normalized','Position',[0.05,0.42,0.5,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','center','Style','text','String','Delta Theta:','BackgroundColor',grasp_env.background_color, 'ForegroundColor', [1 1 1]);
 grasp_handles.window_modules.sector.delta_theta = uicontrol(panel,'units','normalized','Position',[0.6,0.42,0.35,0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(status_flags.analysis_modules.sectors.delta_theta),'HorizontalAlignment','left','Tag','sector_deltatheta','Visible','on','CallBack','sector_callbacks2(''delta_theta'');');
 
-%Angle Binning
+% Angle Binning - for both I vs Q and I bs Xi binning
 uicontrol(panel,'units','normalized','Position',[0.05 0.23 0.5 0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','center','Style','text','String','Angle Bin:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
 grasp_handles.window_modules.radial_average.azimuth_bin = uicontrol(panel,'units','normalized','Position',[0.6 0.23 0.35 0.06],'tooltip','Re-Bin Size in Angle (degrees).  Note: Min Value Depends on the Sector Minimum Radius','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','HorizontalAlignment','left','String', '1', 'Tag','angular_bin','Visible','on','callback','radial_average_callbacks2(''azimuth_bin'');');
 
-% Default Values
-%set(grasp_handles.window_modules.sector.inner_radius,'String',38);
-%set(grasp_handles.window_modules.sector.outer_radius,'String',51);
-%set(grasp_handles.window_modules.sector.theta,'String',304.5);
-%set(grasp_handles.window_modules.sector.delta_theta,'String',50);
-%set(grasp_handles.window_modules.radial_average.azimuth_bin,'String',1.2);
 
-
-%APPLY SECTOR
+% APPLY SECTOR
 uicontrol(panel,'units','normalized','Position',[0.3, 0.07, 0.4, 0.08],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','pushbutton','String','Apply','HorizontalAlignment','center','Tag','boxfox_button','Visible','on','CallBack','mf_GUI_callbacks(''apply_sector'')');
+
 
 %% Smoothing
 
 % smoothing fwhm
 uicontrol(panel2,'units','normalized','Position',[0.05 0.65 0.25 0.25],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','FWHM:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-mf_fitter.handles.smoothing.fwhm = uicontrol(panel2,'units','normalized','Position',[0.05 0.25 0.25 0.3],'tooltip','FWHM for smoothing algorithm','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.smoothing.fwhm),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''smoothing_fwhm'')');
+mf_fitter.handles.smoothing.fwhm = uicontrol(panel2,'units','normalized','Position',[0.05 0.25 0.25 0.3],'tooltip','FWHM for smoothing algorithm','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.user_inputs.smoothing.fwhm),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback','mf_GUI_callbacks(''change_user_input'',''smoothing.fwhm'')');% 'mf_GUI_callbacks(''smoothing_fwhm'')');
 
 % smoothing step size
 uicontrol(panel2,'units','normalized','Position',[0.39 0.65 0.35 0.25],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','Step Size:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-mf_fitter.handles.smoothing.step = uicontrol(panel2,'units','normalized','Position',[0.4 0.25 0.25 0.3],'tooltip','Phi step size for plotting','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.smoothing.step),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''smoothing_stepsize'')');
+mf_fitter.handles.smoothing.step = uicontrol(panel2,'units','normalized','Position',[0.4 0.25 0.25 0.3],'tooltip','Step size for plotting','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.user_inputs.smoothing.step),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback','mf_GUI_callbacks(''change_user_input'',''smoothing.step'')'); %'mf_GUI_callbacks(''smoothing_stepsize'')');
 
-%smoothing on?
+% smoothing checkbox
 uicontrol(panel2,'units','normalized','Position',[0.7 0.65 0.3 0.25],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','Smooth?','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
 mf_fitter.handles.smoothing.switch = uicontrol(panel2,'units','normalized','Position',[0.77 0.27 0.1 0.3],'tooltip','Check if you would like to perform data smoothing','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','checkbox','HorizontalAlignment','right','Tag','angular_bin','Visible','on','Value', 1);
 
 
 %% Other Options
 
-% FITTING PARADIGMS
-%uicontrol(handle,'units','normalized','Position',[0.62 0.8 0.35, 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','peaks move?','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
+% fitting algorithms
 mf_fitter.handles.fit_dropdown = uicontrol('units','normalized','Position',[0.6 0.9, 0.35 0.075],'tooltip','Select a fitting routine','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','popup','string',{'Fitting Paradigms','Peaks Fixed','2K Peaks Grow','14K Peaks Rotate','Raster Scan', 'Angle Binning Scan', 'I vs Q'}, 'Callback', 'mf_GUI_callbacks(''fit_dropdown'')');
-%mf_fitter.handles.dropdown = uicontrol('units','normalized','Position',[0.60 0.03 0.3 0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','popup','string',{'Options','Fraction Plot','Centers Plot','Intensity Colormap','Peak Decay','Angle Binning Scan','Raster Scan'}, 'Callback', 'mf_GUI_callbacks(''dropdown'')');
 
-% PLOT
-%FRACTION, COLORMAP DROPDOWN MENU
-%mf_fitter.handles.dropdown = uicontrol('units','normalized','Position',[0.60 0.03 0.35 0.06],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','popup','string',{'Options','Fraction Plot','Centers Plot','Intensity Colormap','Peak Decay','Angle Binning Scan','Raster Scan'}, 'Callback', 'mf_GUI_callbacks(''dropdown'')');
+% plot maker dropdown 
 mf_fitter.handles.plot_dropdown = uicontrol('units','normalized','Position',[0.60 0.8 0.35 0.075],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','popup','string',{'Plot Options', 'FMS','Peak Centers', 'Peak Separation', 'Intensity Colormap', 'Peak Decay'}, 'Callback', 'mf_GUI_callbacks(''plot_dropdown'')');
 
-
-
-%1 and 2 pk files
+% reference files
 uicontrol(handle,'units','normalized','Position',[0.6 0.66 0.17 0.12],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','1-Peak:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-mf_fitter.handles.good_inner = uicontrol(handle,'units','normalized','Position',[0.6 0.66 0.12 0.06],'tooltip','Select a file with ONE peak','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.good_peaks.inner),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''good_peaks'')');
+mf_fitter.handles.reference_files.one = uicontrol(handle,'units','normalized','Position',[0.6 0.66 0.12 0.06],'tooltip','Select a file with ONE peak','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.user_inputs.reference_files.one),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback','mf_GUI_callbacks(''change_user_input'',''reference_files.one'')'); %'mf_GUI_callbacks(''good_peaks'')');
 
 uicontrol(handle,'units','normalized','Position',[0.75 0.66 0.17 0.12],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','2-Peak:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-mf_fitter.handles.good_outers = uicontrol(handle,'units','normalized','Position',[0.75 0.66 0.12 0.06],'tooltip','Select a file with TWO peaks','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.good_peaks.outers),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''good_peaks'')');
+mf_fitter.handles.reference_files.two = uicontrol(handle,'units','normalized','Position',[0.75 0.66 0.12 0.06],'tooltip','Select a file with TWO peaks','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.user_inputs.reference_files.two),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback','mf_GUI_callbacks(''change_user_input'',''reference_files.two'')'); % 'mf_GUI_callbacks(''good_peaks'')');
 
-%3-peak
+% 3-peak
 uicontrol(handle,'units','normalized','Position',[0.88 0.73 0.3 0.05],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','left','Style','text','String','3-Peak?','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
 mf_fitter.handles.three_peak = uicontrol(handle,'units','normalized','Position',[0.9 0.67 0.048 0.05],'tooltip','Check if there is no good 1-peak reference','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','checkbox','HorizontalAlignment','right','Tag','angular_bin','Visible','on','Value', 0);
 
-
-
-%Peak Percentage Cutoff
+% peak percentage Cutoff
 uicontrol(handle,'units','normalized','Position',[0.6 0.5 0.15 0.12],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','center','Style','text','String','Intensity Cutoff:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-mf_fitter.handles.int_cutoff = uicontrol(handle,'units','normalized','Position',[0.6 0.47 0.15 0.06],'tooltip','Sets minimum fraction of total intensity to be considered a peak','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.int_cutoff),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''change_int_cutoff'')');
+mf_fitter.handles.int_cutoff = uicontrol(handle,'units','normalized','Position',[0.6 0.47 0.15 0.06],'tooltip','Sets minimum fraction of total intensity to be considered a peak','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.user_inputs.int_cutoff),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''change_user_input'',''int_cutoff'')');
 
+% peak separation cutoff 
 uicontrol(handle,'units','normalized','Position',[0.8 0.5 0.15 0.12],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'HorizontalAlignment','center','Style','text','String','Peak Factor:','BackgroundColor', grasp_env.background_color, 'ForegroundColor', [1 1 1]);
-mf_fitter.handles.dphi_cutoff = uicontrol(handle,'units','normalized','Position',[0.8 0.47 0.15 0.06],'tooltip','Multiplicative factor - how much larger than FWHM to be considered for avereages','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.dphi_cutoff),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''change_dphi_cutoff'')');
+mf_fitter.handles.dphi_cutoff = uicontrol(handle,'units','normalized','Position',[0.8 0.47 0.15 0.06],'tooltip','Multiplicative factor - how much larger than FWHM to be considered for avereages','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','edit','String',num2str(mf_fitter.user_inputs.dphi_cutoff),'HorizontalAlignment','left','Tag','angular_bin','Visible','on','Callback', 'mf_GUI_callbacks(''change_user_input'',''dphi_cutoff'')');
 
-
-
-%FIT TABLE BUTTON
+% fit table button
 uicontrol('units','normalized','Position',[0.625 0.32 0.3 0.1],'FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','pushbutton','string','View Params', 'Callback', 'mf_fitter_table');
 
 % Experiment & Save Options
@@ -130,11 +123,10 @@ uicontrol(handle,'units','normalized','Position',[0.84 0.26 0.35 0.05],'FontName
 mf_fitter.handles.save = uicontrol(handle,'units','normalized','Position',[0.85 0.215 0.048 0.05],'tooltip','Check if you want to auto save files based on the save options','FontName',grasp_env.font,'FontSize',grasp_env.fontsize,'Style','checkbox','HorizontalAlignment','right','Tag','angular_bin','Visible','on','Value', 0);
     
 
-%LOOP BUTTON
+%% GO!
+% perform fitting algorithm button
 uicontrol('units','normalized','Position',[0.675 0.03 0.2 0.15],'FontName',grasp_env.font,'FontSize',1.25*grasp_env.fontsize,'Style','pushbutton','string','GO!','Callback','mf_GUI_callbacks(''go'')');
    
-
-
 
 %% Old options 
 

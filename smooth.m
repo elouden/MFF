d09d7042551e55fb40607d8d7af1ca725102cd53
Elizabeth R Louden <edewaard@nd.edu>
2018-03-01 21:07:27 -0500

    Add files via upload
    Cleaned up the smoothing function, simplified the centering procedure, introduced better comments and a top function description.  This function does not use any portion of the data structure, so those changes will not affect this function

--- smooth.m
+++ smooth.m
@@ -1,79 +1,80 @@
-function [phiS, intS, intErrS] = smooth( nphi, nInt, nIntErr, fn, phisym, symtrz, phirng, gwidth) %, intrng, savfig, clsfig, hl, fig_h, cm, cm_x, path)   
-%% Allocating memory for processed data
+function [phiS, intS, intErrS] = smooth( nphi, nInt, nIntErr, fn, phisym, symtrz, phirng, gwidth) 
+% v 9.2 2/21/2018 E R Louden    
+
+% function [phiS, intS, intErrS] = smooth( nphi, nInt, nIntErr, fn, phisym, symtrz, phirng, gwidth) 
+% smooth:   performs binning and smoothing on raw neutron counts
+%           exported from GRASP with an angle binning of 0.1 (see function getData
+%           for the processof exporting).  
+%           The original verion of this function was drafted by M R Eskildsen.
+%           Data can either by a single vector of ints, or a matrix of int vs some control parameter
+%   
+%   phiS        -    smoothed/binned x values, NUM
+%   intS        -    smoothed/binned y values, NUM
+%   ntErrS      -    error on y, NUM
+%
+%   nphi        -    initial x values, ideally exported with AB = 0.1, NUM 
+%   nInt        -    initial y values, NUM
+%   nIntErr     -    initial error on y, NUM
+%   fn          -    the number of passed y data sets if giving a matrix of   y, otherwise 1, INT
+%   physym      -    the value to center data at, NUM
+%   symtrz      -    if data should be symmetrized, input 1 to reflect data about physym, INT     
+%   phirng      -    the final x values to plot against, [NUM (start), NUM (end), NUM (step size)] 
+%   gwidth      -    the guassian fwhm determining smoothing amount, NUM
+
+
 N = numel(fn);
 phi = phirng(1):phirng(3):phirng(2);
 Int = zeros([N length(phi)]);
 IntErr = Int;
       
-for n = 1:N
-% center phi range around zero (subtract 360 from values > 180)
-%     data = readtable([pathname char(fn(n))],'HeaderLines',hl(n),'Delimiter','\t'); 
-%     nphi = data{:,{'Az_Angle'}};
-%     nInt = data{:,{'I'}};
-%     nIntErr = data{:,{'Err_I'}};
-%     
-%     %center about I-phase position (stored in phisym)
+    for n = 1:N
+        
+        % Center data about phisym
 
-%      nphi = nphi - phisym
-%       
-     phiabove = find(nphi > 180);
-%     phisym
-     if(phisym(n) > 180)
-         j = 1;
-        if(length(phisym) == 1) pnum = 1; else pnum = n;   end
-         nphi = nphi - phisym(pnum)
-     else
-         nphi(phiabove) = nphi(phiabove) - 360;
-%     
-    %center about I-phase position (stored in phisym)
-     j = 0;
-% %     if(phisym > 180)
-% %         j = 1;
-% %     end
-         if(length(phisym) == 1) pnum = 1; else pnum = n;   end
-%         phisym(pnum)
-         max(nphi)
-         min(nphi)
-         nphi = nphi - phisym(pnum) + j*360;
-     end
-%     
-    % Symmetrize
-    if(symtrz);
-        nphi = [nphi-phisym(n); -(nphi-phisym(n))];
-        nInt = [nInt; nInt];
-        nIntErr = [nIntErr; nIntErr];
+        % GRASP will output data above 360 as being at 1 - check for this case
+        % if it did, the sorted phi will not match input nphi
+        nphiSORT = sort(nphi);
+        if(nphiSORT ~= nphi)
+            nphi(find(nphi < nphi(1))) = nphi(find(nphi < nphi(1))) + 360;
+        end
+
+        nphi = nphi - phisym;
+
+        
+        % Symmetrize
+        if(symtrz);
+            nphi = [nphi-phisym(n); -(nphi-phisym(n))];
+            nInt = [nInt; nInt];
+            nIntErr = [nIntErr; nIntErr];
+        end
+
+        
+         % Smooth
+
+         WhtInt = Int(1,:); % weighted intensity
+         WhtIntErrSq = WhtInt; % weighted error
+         Wht = WhtInt;  % total weight for normalizing
+
+         % loop over the desired bin locations
+         for i = 1:length(phi)
+
+             % for each bin, sum up the intensity based on weighted average for distance from nominal bin position 
+             for j = 1:length(nphi)
+                 WhtInt(j) = exp(-(phi(i)-nphi(j))^2/gwidth^2)*nInt(j);
+                 WhtIntErrSq(j) = (exp(-(phi(i)-nphi(j))^2/gwidth^2)*nIntErr(j))^2;
+                 Wht(j) = exp(-(phi(i)-nphi(j))^2/gwidth^2);
+             end
+
+             % The final intensity at this phi is the sum of the weighted intensity divided by the total weight
+             Int(n,i) = sum(WhtInt)/sum(Wht);
+             IntErr(n,i) = sqrt(sum(WhtIntErrSq))/sum(Wht);
+         end
+
+         % once smoothing is done, store the final variables into the output variables
+         phiS = phi;
+         intS = Int;
+         intErrS = IntErr;
+
     end
     
-     % Smooth
-     WhtInt = Int(1,:);
-     WhtIntErrSq = WhtInt;
-     Wht = WhtInt;
-%     if(phisym > 180)
-%         disp('break')
-%     end
-     for i = 1:length(phi)
-         for j = 1:length(nphi)
-             WhtInt(j) = exp(-(phi(i)-nphi(j))^2/gwidth^2)*nInt(j);
-             WhtIntErrSq(j) = (exp(-(phi(i)-nphi(j))^2/gwidth^2)*nIntErr(j))^2;
-             Wht(j) = exp(-(phi(i)-nphi(j))^2/gwidth^2);
-         end
-         length(fn)
-         phisym
-         if(phisym(n) > 180 & length(fn)>1)
-             disp('break')
-         end
-         Int(n,i) = sum(WhtInt)/sum(Wht);
-         IntErr(n,i) = sqrt(sum(WhtIntErrSq))/sum(Wht);
-     end
-%     
-       phiS = phi;
-       intS = Int;
-       intErrS = IntErr;
-%     %Store in data structure
-%     name = ['Field_' char(fh(n))];
-%     ILL_Dat.(name).phi = phi;
-%     ILL_Dat.(name).Int = Int(n,:);
-%     ILL_Dat.(name).IntErr = IntErr(n,:);
-%     
-end
 end
